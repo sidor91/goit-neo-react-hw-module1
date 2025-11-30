6dc545d2b784feb5dd1d946511a9cf8531bcdac8
serhiisydorenko-wdl <sidorchik91@gmail.com>
2025-11-30 21:15:09 +0100

    update json location

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import Profile from './components/Profile/Profile';
-import userData from './components/Profile/userData.json';
 import FriendList from './components/FriendList/FriendList';
-import friends from './components/FriendList/friends.json';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './components/TransactionHistory/transactions.json';
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 const App = () => {
   return (
